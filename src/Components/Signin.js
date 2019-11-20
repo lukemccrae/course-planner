32b8cb8f4513d643d4a9e1b9dfed3d6bd7dff2f7
“lukemccrae” <lukemccrae@gmail.com>
2019-11-20 14:22:37 -0800

    http call changed in signin

--- src/Components/Signin.js
+++ src/Components/Signin.js
@@ -36,9 +36,8 @@ class Signin extends Component {
 
   onSignIn(e) {
     //turn this on when pushing
-    // https://banana-crumble-42815.herokuapp.com
     e.preventDefault();
-    fetch(`http://localhost:3000/api/account/signin`, {
+    fetch(`https://banana-crumble-42815.herokuapp.com/api/account/signin`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
