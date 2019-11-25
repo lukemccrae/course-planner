5b7c6ce8909b6c56b9b1a2cd057cc487bb29f91b
“lukemccrae” <lukemccrae@gmail.com>
2019-11-25 11:05:50 -0800

    sort stats

--- src/Components/Stats.js
+++ src/Components/Stats.js
@@ -11,28 +11,38 @@ const StatBox = styled.div`
 
 const Stats = ({log}) => {
     let counter = [];
+
     function Stat(name, length, key) {
         this.name = name;
         this.length = length;
         this.key = key;
     }
+
+    //is this log entry already in the counter?
     function findLog(entry) {
-        let result = counter.map(function(l) { return l.name; }).indexOf(entry); 
-        return result;
+        return counter.map(function(l) { return l.name; }).indexOf(entry); 
     }
+
     for (let i = 0; i < log.length; i++) {
+
+        //if its not there,
         if(findLog(log[i].name) === -1) {
+            //push a new stat entry
             let stat1 = new Stat(log[i].name, log[i].length, i)
-            counter.push(stat1)
+            counter.push(stat1);
         } else {
             //increment hash map log value
             counter[findLog(log[i].name)].length += log[i].length
         }
     }
+
+    //sort logs
+    counter.sort((a, b) => parseFloat(b.length) - parseFloat(a.length));
+
+    
     
     return (
         <div>{counter.map(l => {
-            
             return (
                 <StatBox key={l.key}>
                     <div>{l.name}</div>
