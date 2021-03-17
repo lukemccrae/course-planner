13ceba73a9facc85f4b8d56a011bf75506252cd7
“lukemccrae” <lukemccrae@gmail.com>
2021-03-17 15:29:45 -0700

    removed set timeout

--- src/Components/CountdownCircle.js
+++ src/Components/CountdownCircle.js
@@ -87,42 +87,43 @@ class CountdownCircle extends Component {
 
       //this is called with state updates
       //so after every setTimeout (many many times)
-      componentDidUpdate () {
-        let numberTicks = 5;
+    //   componentDidUpdate () {
+    //     let numberTicks = 5;
 
-        //set up amount to move spinner each second
-        let currentIncrement = 100 / this.props.timer.length / numberTicks;
+    //     //set up amount to move spinner each second
+    //     let currentIncrement = 100 / this.props.timer.length / numberTicks;
         
-        //make currentIncrement 0 if timer name is undefined
-        if(this.props.timer.name === undefined) currentIncrement = 0;
+    //     //make currentIncrement 0 if timer name is undefined
+    //     if(this.props.timer.name === undefined) currentIncrement = 0;
 
-        //if spinner isnt complete, or wont be complete after this tick,
-        if (this.state.currentPercent + currentIncrement < 100) {
-            currentIncrement += currentIncrement * this.state.tickAdjust;
-            let currentPercent = this.state.currentPercent;
+    //     //if spinner isnt complete, or wont be complete after this tick,
+    //     if (this.state.currentPercent + currentIncrement < 100) {
+    //         currentIncrement += currentIncrement * this.state.tickAdjust;
+    //         let currentPercent = this.state.currentPercent;
 
-            setTimeout(() => 
-            this.setState({
-                currentPercent: currentPercent + currentIncrement
-            }), 200);
+    //         setTimeout(() => 
+    //         this.setState({
+    //             currentPercent: currentPercent + currentIncrement
+    //         }), 200);
 
-        //once the timer is finished, make the final color white
-        //this solves the problem of the spinner not totally completing if the increment bumps it over 100%
-        } else {
-            //set state colors array in variable
-            let colorsDoneBecomeWhite = this.props.colors;
+    //     //once the timer is finished, make the final color white
+    //     //this solves the problem of the spinner not totally completing if the increment bumps it over 100%
+    //     } else {
+    //         console.log('hi')
+    //         //set state colors array in variable
+    //         let colorsDoneBecomeWhite = this.props.colors;
 
-            let index = this.props.group.timers.indexOf(this.props.currentTimer);
-            for (let i = 0; i < index + 1; i++) {
-                if(this.props.colors[i] !== "white") {
-                    colorsDoneBecomeWhite[i] = "white";
-                    this.setState({
-                        colors: colorsDoneBecomeWhite
-                    })
-                }
-            }
-        }
-      }
+    //         let index = this.props.group.timers.indexOf(this.props.currentTimer);
+    //         for (let i = 0; i < index + 1; i++) {
+    //             if(this.props.colors[i] !== "white") {
+    //                 colorsDoneBecomeWhite[i] = "white";
+    //                 this.setState({
+    //                     colors: colorsDoneBecomeWhite
+    //                 })
+    //             }
+    //         }
+    //     }
+    //   }
 
       //this is an attempt to adjust the spinning rate to compensate for setTimeout bugs
       //currently this.state.currentPercent and props.percent are not synchronized
@@ -163,12 +164,16 @@ class CountdownCircle extends Component {
         let firstCalculatedPercent = 270;
 
         //white gradient on the right, moves first
-        let secondCalculatedPercent = 90 + this.state.currentPercent * 3.6;
+        //this is how its done with set timeout changing state value
+        // let secondCalculatedPercent = 90 + this.state.currentPercent * 3.6;
+
+        //this is how i can do it without set timeout but only moving once per second
+        let secondCalculatedPercent = 90 + this.props.percent * 3.6;
 
         //once spinner is halfway done, colors switch
         if(secondCalculatedPercent >= 270) {
             secondCalculatedPercent = 270;
-            firstCalculatedPercent = 270 + this.state.currentPercent * 3.6;
+            firstCalculatedPercent = 270 + this.props.percent * 3.6;
         }
 
         return (
