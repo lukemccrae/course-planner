e19abf24f577d9682e58b2cee5622d6aab784aa7
“lukemccrae” <lukemccrae@gmail.com>
2021-04-06 20:15:57 -0700

    displaycircle functional

--- src/Components/DisplayCircle.js
+++ src/Components/DisplayCircle.js
@@ -1,18 +1,6 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import Completionist from './Completionist';
-
-// let colors = [
-//     "#428A79",
-//     "#71AF55",
-//     "#F00500",
-//     "#E4BE67",
-//     "#E47043",
-//     "#B63534",
-//     "#9598AB",
-//     "#697C68",
-//     "#420C0A"
-// ].sort(() => .5 - Math.random());
 
 
 const TimeBox = styled.div`
@@ -37,27 +25,28 @@ const Circle = styled.div`
 
     // set width / height as functions of the index of each timer in the timers group array
     // this enables them to sit on top of each other
-    width: ${(props) => 42 - props.children._owner.stateNode.props.group.timers.indexOf(props.timer) * 4 + "vh"};
-    height: ${(props) => 42 - props.children._owner.stateNode.props.group.timers.indexOf(props.timer) * 4 + "vh"};
+    width: ${(props) => console.log(props.children)};
+    width: ${(props) => 42 - props.children._owner.memoizedProps.group.timers.indexOf(props.timer) * 4 + "vh"};
+    height: ${(props) => 42 - props.children._owner.memoizedProps.group.timers.indexOf(props.timer) * 4 + "vh"};
     border-radius: 100%;
-    background-color: ${(props) => props.colors[props.children._owner.stateNode.props.group.timers.indexOf(props.timer)]};
+    background-color: ${(props) => props.colors[props.children._owner.memoizedProps.group.timers.indexOf(props.timer)]};
     background-image:
 
         //to ensure that only one circle counts down at once, the degree passed to linear gradient needs to either be the correct one or 270 / 90, which will keep it unmoving
         //if (parent).props.currentTimer is props.timer, give it the right percent. otherwise give it 270 / 90
 
-        linear-gradient(${(props) => (props.timer === props.children._owner.stateNode.props.currentTimer ? props.firstPercent : 270) + 'deg'}, transparent 50%, ${(props) => props.firstGradientColor} 50%),
-        linear-gradient(${(props) => (props.timer === props.children._owner.stateNode.props.currentTimer ? props.secondPercent : 90) + 'deg'}, white 50%, transparent 50%);
+        linear-gradient(${(props) => (props.timer === props.children._owner.memoizedProps.currentTimer ? props.firstPercent : 270) + 'deg'}, transparent 50%, ${(props) => props.firstGradientColor} 50%),
+        linear-gradient(${(props) => (props.timer === props.children._owner.memoizedProps.currentTimer ? props.secondPercent : 90) + 'deg'}, white 50%, transparent 50%);
 
     align-items: center;
     justify-content: center;
 
     margin: 
     //margin top and left porportional to its place in the timer array
-        ${(props) => props.children._owner.stateNode.props.group.timers.indexOf(props.timer) * 2 + "vh"};
+        ${(props) => props.children._owner.memoizedProps.group.timers.indexOf(props.timer) * 2 + "vh"};
         0
         0
-        ${(props) => props.children._owner.stateNode.props.group.timers.indexOf(props.timer) * 2 + "vh"};
+        ${(props) => props.children._owner.memoizedProps.group.timers.indexOf(props.timer) * 2 + "vh"};
 
     display: flex;
 `
@@ -70,121 +59,58 @@ const InnerCircle = styled.div`
 
     //height and width are functions of how many timers there are
     //  decrease circle for each timer
-    width: ${(props) => 39 - props.children._owner.stateNode.props.group.timers.indexOf(props.timer) * 4 + "vh"};
-    height: ${(props) => 39 - props.children._owner.stateNode.props.group.timers.indexOf(props.timer) * 4 + "vh"};
+    width: ${(props) => 39 - props.children._owner.memoizedProps.group.timers.indexOf(props.timer) * 4 + "vh"};
+    height: ${(props) => 39 - props.children._owner.memoizedProps.group.timers.indexOf(props.timer) * 4 + "vh"};
     background-color: white;
 `
 
 
-class CountdownCircle extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            colors: [
-                "#428A79",
-                "#71AF55",
-                "#F00500",
-                "#E4BE67",
-                "#E47043",
-                "#B63534",
-                "#9598AB",
-            ],
-            //randomize array (couldnt get it to work this way)
-            // .sort(() => .5 - Math.random())
-            currentPercent: 0,
-            tickAdjust: .01
-        }
-      }
+function displayCircle(props) {
+    console.log(props)
 
-      //this is called with state updates
-      //so after every setTimeout (many many times)
-      componentDidUpdate () {
-        let numberTicks = 5;
+      const [colors, setColors] = useState([
+        "#428A79",
+        "#71AF55",
+        "#F00500",
+        "#E4BE67",
+        "#E47043",
+        "#B63534",
+        "#9598AB",]);
 
-        //set up amount to move spinner each second
-        let currentIncrement = 100 / this.props.timer.length / numberTicks;
-        
-        //make currentIncrement 0 if timer name is undefined
-        if(this.props.timer.name === undefined) currentIncrement = 0;
+        const[currentPercent, setPercent] = useState(0);
 
-        //if spinner isnt complete, or wont be complete after this tick,
-        if (this.state.currentPercent + currentIncrement < 100) {
-            currentIncrement += currentIncrement * this.state.tickAdjust;
-            let currentPercent = this.state.currentPercent;
 
-            setTimeout(() => 
-            this.setState({
-                currentPercent: currentPercent + currentIncrement
-            }), 200);
-
-        //once the timer is finished, make the final color white
-        //this solves the problem of the spinner not totally completing if the increment bumps it over 100%
-        } else {
-            //set state colors array in variable
-            let colorsDoneBecomeWhite = this.state.colors;
-
-            let index = this.props.group.timers.indexOf(this.props.currentTimer);
-            for (let i = 0; i < index + 1; i++) {
-                if(this.state.colors[i] !== "white") {
-                    colorsDoneBecomeWhite[i] = "white";
-                    this.setState({
-                        colors: colorsDoneBecomeWhite
-                    })
-                }
-            }
-        }
-      }
-
-      //this is an attempt to adjust the spinning rate to compensate for setTimeout bugs
-      //currently this.state.currentPercent and props.percent are not synchronized
-      componentWillReceiveProps(nextProps) {
-        //   console.log(nextProps.percent, this.state.currentPercent)
-        //   let newTickAdjust = this.state.tickAdjust;
-        // if(this.state.currentPercent % nextProps.percent > .05){
-        //     this.setState({
-        //         tickAdjust: newTickAdjust - .05
-        //     })
-        // } else {
-        //     this.setState({
-        //         tickAdjust: newTickAdjust + .1
-        //     })
-        // }
-    }
 
       //make finished timers colors white so they disapear after done
       //this runs when component starts, either on group start or next timer
-      UNSAFE_componentWillMount() {
+      useEffect(() => {
         //copy state colors array
-        let colorsDoneBecomeWhite = this.state.colors;
-        let index = this.props.group.timers.indexOf(this.props.currentTimer);
+        let colorsDoneBecomeWhite = colors;
+        let index = props.group.timers.indexOf(props.currentTimer);
 
         for (let i = 0; i < index; i++) {
-            if(this.state.colors[i] !== "white") {
+            if(colors[i] !== "white") {
                 colorsDoneBecomeWhite[i] = "white";
-                this.setState({
-                    colors: colorsDoneBecomeWhite,
-                    currentTotalSeconds: this.props.seconds
-                })
+                setColors(colorsDoneBecomeWhite)
             }
         }
-      }
+      }, []);
     
-    render() {
         //white gradient on the left
         let firstCalculatedPercent = 270;
 
         //white gradient on the right, moves first
-        let secondCalculatedPercent = 90 + this.state.currentPercent * 3.6;
+        let secondCalculatedPercent = 90 + currentPercent * 3.6;
 
         //once spinner is halfway done, colors switch
         if(secondCalculatedPercent >= 270) {
             secondCalculatedPercent = 270;
-            firstCalculatedPercent = 270 + this.state.currentPercent * 3.6;
+            firstCalculatedPercent = 270 + currentPercent * 3.6;
         }
 
         return (
             <div>
-                {this.props.group.timers.map(t => {
+                {props.group.timers.map(t => {
                     return (
                         <Circle 
                         //only give the updated percent if the timerStart boolean is on
@@ -194,22 +120,22 @@ class CountdownCircle extends Component {
                             //if the spinner is over halfway, only make the spinning color change to white
                             firstGradientColor={secondCalculatedPercent >= 270 ? 
                                 //if timer is spinning timer, make it white after halfway
-                                (this.props.currentTimer === t ? "white" : this.state.colors[this.props.group.timers.indexOf(t)]) : this.state.colors[this.props.group.timers.indexOf(t)]}
+                                (props.currentTimer === t ? "white" : colors[props.group.timers.indexOf(t)]) : colors[props.group.timers.indexOf(t)]}
                             timer={t}
-                            colors={this.state.colors}
+                            colors={colors}
                         >
                     
                         <InnerCircle
                             timer={t}
                         >
                         {
-                            this.props.completed ? 
-                            <Completionist currentColor={this.state.colors[this.props.group.timers.indexOf(this.props.currentTimer)]} group={this.props.group} userId={this.props.userId} getTimers={this.props.getTimers} currentTimer={this.props.currentTimer} nextTimer={this.props.nextTimer}></Completionist>
+                            props.completed ? 
+                            <Completionist currentColor={colors[props.group.timers.indexOf(props.currentTimer)]} group={props.group} userId={props.userId} getTimers={props.getTimers} currentTimer={props.currentTimer} nextTimer={props.nextTimer}></Completionist>
                         :
                         <div>
-                            {this.props.timerStart ? <TimeBox>
-                                <Name>{this.props.timer.name}</Name>
-                                <Time>{this.props.minutes}:{this.props.seconds}</Time>
+                            {props.timerStart ? <TimeBox>
+                                <Name>{props.timer.name}</Name>
+                                <Time>{props.minutes}:{props.seconds}</Time>
                             </TimeBox> : null}
                             
                         </div>
@@ -224,6 +150,5 @@ class CountdownCircle extends Component {
             </div>
         )
     }
-}
 
-export default CountdownCircle;
+export default displayCircle;
